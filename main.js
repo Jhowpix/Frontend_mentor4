b84ecf7faa30b2f44cb71b85790335a9797fb19f
Jhonatan de Oliveira <jhonatandevbr@gmail.com>
2023-05-12 21:31:20 +0900

    .
    .

---
+++ main.js
@@ -0,0 +1,11 @@
+function clickMenu(){
+
+   const navMenu = document.getElementById('nave');
+
+  if(navMenu.style.display == 'block'){
+    navMenu.style.display = 'none'
+  } else{
+    navMenu.style.display = 'block'
+  }
+  
+}
